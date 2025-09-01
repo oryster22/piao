d1c64edcc9f90a361af5fbcd96b646fcc62040da
oryster22 <aqpk27@gmail.com>
2025-09-02 06:05:14 +0700

    Delete veo3api directory

--- veo3api/lib/encoder.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async enc({ data }) {
-    return { uuid: Buffer.from(JSON.stringify(data)).toString("base64") };
-  },
-  async dec({ uuid }) {
-    return { text: JSON.parse(Buffer.from(uuid, "base64").toString()) };
-  }
-};
