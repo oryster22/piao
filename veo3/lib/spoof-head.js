4ab9e1d6c2190af179b80623bad7af2761e7a6a7
oryster22 <aqpk27@gmail.com>
2025-09-02 06:45:28 +0700

    Add files via upload

---
+++ veo3/lib/spoof-head.js
@@ -0,0 +1,8 @@
+/**
+ * Fungsi ini dapat Anda kembangkan untuk menambahkan header kustom
+ * untuk meniru atau "spoof" berbagai jenis browser atau perangkat.
+ * Untuk saat ini, akan mengembalikan objek kosong agar kode tetap berjalan.
+ */
+export default function SpoofHead() {
+  return {};
+}
