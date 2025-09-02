ad39bbe5a3d808f583c61ad64b86305a871eb6d9
oryster22 <aqpk27@gmail.com>
2025-09-02 08:52:46 +0700

    Update apiConfig.js

--- veo3/configs/apiConfig.js
+++ veo3/configs/apiConfig.js
@@ -1,7 +1,8 @@
 const apiConfig = {
   // Ganti dengan domain temp mail yang Anda gunakan jika berbeda.
   // '1secmail.com' atau 'tempmail.io' adalah contoh umum.
-  DOMAIN_URL: "mail.tm"
+  DOMAIN_URL: "tempimail.org"
 };
+
 
 export default apiConfig;
