9dba578bc8061f8b5d686cb2e86cceef948925bb
oryster22 <aqpk27@gmail.com>
2025-09-02 09:34:08 +0700

    Delete veo3 directory

--- veo3/configs/apiConfig.js
+++
@@ -1,8 +0,0 @@
-const apiConfig = {
-  // Ganti dengan domain temp mail yang Anda gunakan jika berbeda.
-  // '1secmail.com' atau 'tempmail.io' adalah contoh umum.
-  DOMAIN_URL: "tempimail.org"
-};
-
-
-export default apiConfig;
